b93ba9738ec59070f1a5f1cef4ae39c5668dce8a
Ramakrishna Parsekar <Ramakrishna.Parsekar@wockhardt.net>
2019-08-17 14:56:16 +0530

    more changes

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 def addone(num):
     return num+1
 def square(num):
-    return num **2 
+    return num **2
+def cube(num):
+    return num**3 
     
 if __name__ == "__main__":
     print(addone(1))
     print(square(1))
+    print(cube(1))
